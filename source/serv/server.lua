59866cb1f8b464e20957c16f4779215cb3b89971
Stoic <112611821+TheStoicBear@users.noreply.github.com>
2024-02-04 12:27:29 -0800

    Update server.lua

--- source/serv/server.lua
+++ source/serv/server.lua
@@ -6,93 +6,43 @@ function CalculateDistance(x1, y1, z1, x2, y2, z2)
     return math.sqrt(dx * dx + dy * dy + dz * dz)
 end
 
-lib.addCommand("adrev", {
-    help = "Admin command, revive player.",
-    restricted = "group.admin",
-    params = {
-        {
-            name = "target",
-            type = "playerId",
-            help = "Target player's server id"
-        }
-    }
-}, function(source, args, raw)
-    local player = NDCore.getPlayer(args.target)
+QBCore.Commands.Add("adrev", "Admin command, revive player.", {{name="target", help="Target player's server id"}}, function(source, args)
+    local player = QBCore.Functions.GetPlayer(args[1])
 
     if not player then
         print("[adrev] Error: Player not found.")
-        local adminPlayer = NDCore.getPlayer(source)
-        adminPlayer.notify({
-            title = "Error",
-            position = 'top',  -- Set the position of the notification
-            description = "Invalid player ID.",
-            type = "error",
-            duration = 5000
-        })
+        local adminPlayer = QBCore.Functions.GetPlayer(source)
+        adminPlayer.Functions.Notify("Invalid player ID.", "error")
         return
     end
 
-    local targetPlayerId = tonumber(args.target)
+    local targetPlayerId = tonumber(args[1])
 
     if targetPlayerId then
-        local targetPlayerId = NDCore.getPlayer(targetPlayerId)
-        targetPlayerId.revive()
-        TriggerClientEvent("AdminRevivePlayerAtPosition", targetPlayerId)
-        player.notify({
-            title = "Admin Action",
-            position = 'top',  -- Set the position of the notification
-            description = "You have been revived by an admin.",
-            type = "success",
-            duration = 5000
-        })
+        local targetPlayer = QBCore.Functions.GetPlayer(targetPlayerId)
+        targetPlayer.Functions.Revive()
+        TriggerClientEvent("AdminRevivePlayerAtPosition", targetPlayer.PlayerData.source)
+        player.Functions.Notify("You have been revived by an admin.", "success")
 
-        local adminPlayer = NDCore.getPlayer(source)
-        adminPlayer.notify({
-            title = "Admin Action",
-            position = 'top',  -- Set the position of the notification
-            description = "You have revived player " .. targetPlayerId,
-            type = "success",
-            duration = 5000
-        })
+        local adminPlayer = QBCore.Functions.GetPlayer(source)
+        adminPlayer.Functions.Notify("You have revived player " .. targetPlayerId, "success")
     else
-        local adminPlayer = NDCore.getPlayer(source)
-        adminPlayer.notify({
-            title = "Error",
-            position = 'top',  -- Set the position of the notification
-            description = "Invalid player ID.",
-            type = "error",
-            duration = 5000
-        })
+        local adminPlayer = QBCore.Functions.GetPlayer(source)
+        adminPlayer.Functions.Notify("Invalid player ID.", "error")
     end
-end)
+end, "admin")
 
-lib.addCommand("cpr", {
-    help = "Medic command, perform CPR on a player.",
-    restricted = "medic", -- Change to the appropriate group or remove for no restriction
-    params = {
-        {
-            name = "target",
-            type = "playerId",
-            help = "Target player's server id"
-        }
-    }
-}, function(source, args, raw)
-    local player = NDCore.getPlayer(source)
+QBCore.Commands.Add("cpr", "Medic command, perform CPR on a player.", {{name="target", help="Target player's server id"}}, function(source, args)
+    local player = QBCore.Functions.GetPlayer(source)
 
     if not player then
         print("[CPR] Error: Character data not found for source player.")
-        return player.notify({
-            title = "Error",
-            position = 'top',  -- Set the position of the notification
-            description = "Character data not found.",
-            type = "error",
-            duration = 5000
-        })
+        return player.Functions.Notify("Character data not found.", "error")
     end
 
     local hasPermission = false
     for _, department in pairs(Config.MedDept) do
-        if player.job == department then
+        if player.PlayerData.job.name == department then
             hasPermission = true
             break
         end
@@ -100,53 +50,29 @@ lib.addCommand("cpr", {
 
     if not hasPermission then
         print("[CPR] Error: Player does not have permission to use this command.")
-        return player.notify({
-            title = "Error",
-            position = 'top',  -- Set the position of the notification
-            description = "You don't have permission to use this command.",
-            type = "error",
-            duration = 5000
-        })
+        return player.Functions.Notify("You don't have permission to use this command.", "error")
     end
 
-    local targetPlayerId = tonumber(args.target)
+    local targetPlayerId = tonumber(args[1])
 
     if not targetPlayerId then
         print("[CPR] Error: Invalid player ID.")
-        return player.notify({
-            title = "Error",
-            position = 'top',  -- Set the position of the notification
-            description = "Invalid player ID.",
-            type = "error",
-            duration = 5000
-        })
+        return player.Functions.Notify("Invalid player ID.", "error")
     end
 
-    local targetPlayer = NDCore.getPlayer(targetPlayerId)
+    local targetPlayer = QBCore.Functions.GetPlayer(targetPlayerId)
     if not targetPlayer then
         print("[CPR] Error: Character data not found for the target player.")
-        return player.notify({
-            title = "Error",
-            position = 'top',  -- Set the position of the notification
-            description = "Character data not found for the target player.",
-            type = "error",
-            duration = 5000
-        })
+        return player.Functions.Notify("Character data not found for the target player.", "error")
     end
 
     local maxDistance = 5.0 -- Change this value to your desired maximum distance
 
-    local playerPed = GetPlayerPed(source)
+    local playerPed = PlayerPedId()
 
     if not DoesEntityExist(playerPed) then
         print("[CPR] Error: Invalid player entity. PlayerPed is nil.")
-        return player.notify({
-            title = "Error",
-            position = 'top',  -- Set the position of the notification
-            description = "Invalid player entity.",
-            type = "error",
-            duration = 5000
-        })
+        return player.Functions.Notify("Invalid player entity.", "error")
     end
 
     local playerCoords = GetEntityCoords(playerPed)
@@ -154,16 +80,10 @@ lib.addCommand("cpr", {
 
     if not playerCoords or not targetCoords then
         print("[CPR] Error: Invalid player positions.")
-        return player.notify({
-            title = "Error",
-            position = 'top',  -- Set the position of the notification
-            description = "Invalid player positions.",
-            type = "error",
-            duration = 5000
-        })
+        return player.Functions.Notify("Invalid player positions.", "error")
     end
 
-    local distance = #(playerCoords - targetCoords)
+    local distance = CalculateDistance(playerCoords.x, playerCoords.y, playerCoords.z, targetCoords.x, targetCoords.y, targetCoords.z)
 
     print("[CPR] Debug: Distance between players:", distance)
 
@@ -172,35 +92,17 @@ lib.addCommand("cpr", {
         Citizen.Wait(5000)
 
         local cprMessage = "You have initiated CPR!"
-        player.notify({
-            title = "Medical Action",
-            position = 'top',  -- Set the position of the notification
-            description = cprMessage,
-            type = "inform",
-            duration = 5000
-        })
+        player.Functions.Notify(cprMessage, "inform")
 
         TriggerClientEvent("ND_Death:CPR", targetPlayerId)
 
         local reviveMessage = "You have revived a Player."
-        player.notify({
-            title = "Medical Action",
-            position = 'top',  -- Set the position of the notification
-            description = reviveMessage,
-            type = "success",
-            duration = 5000
-        })
+        player.Functions.Notify(reviveMessage, "success")
     else
         print("[CPR] Error: Target player is too far away.")
-        player.notify({
-            title = "Error",
-            position = 'top',  -- Set the position of the notification
-            description = "Target player is too far away.",
-            type = "error",
-            duration = 5000
-        })
+        player.Functions.Notify("Target player is too far away.", "error")
     end
-end)
+end, "medic")
 
 RegisterServerEvent("ND_Death:AdminRevivePlayerAtPosition")
 AddEventHandler("ND_Death:AdminRevivePlayerAtPosition", function(targetPlayerId)
@@ -220,10 +122,10 @@ end)
 
 RegisterServerEvent("PlayerDownNotification")
 AddEventHandler("PlayerDownNotification", function(streetName, crossingRoad)
-    local player = NDCore.getPlayer(source)
+    local player = QBCore.Functions.GetPlayer(source)
     
     if player then
-        local playerJob = player.job
+        local playerJob = player.PlayerData.job.name
 
         -- Check if the player's job is in MedDept
         for _, job in pairs(Config.MedDept) do
